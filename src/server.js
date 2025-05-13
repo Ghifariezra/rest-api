4d5362954ac0880e12858b002e6411bf78df8256
Owner <ghifariezraramadhan@gmail.com>
2025-05-13 21:58:24 +0700

    full update

--- src/server.js
+++ src/server.js
@@ -1,12 +1,22 @@
 import express from "express";
+import cors from "cors";
 import routes from "./routes/routes.js";
 import logs from "./middleware/logs.js";
+import path from "path";
 
 const PORT = 4000;
+const publicPath = path.join(process.cwd(), "src", "public", "assets");
 const app = express();
 
+app.use(cors(
+    {
+        origin: "https://ghifariezraramadhan-portofolio.vercel.app/",
+        methods: ["GET"],
+        credentials: true
+    }
+));
 app.use(express.json());
-
+app.use("/assets", express.static(publicPath));
 app.use(logs);
 app.use(routes);
 
